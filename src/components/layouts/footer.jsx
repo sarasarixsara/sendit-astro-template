dda305821cc07807228f2cd63487566e96c5a36a
sarasarixsara <sara.cuy.ramirez@gmail.com>
2024-01-19 13:42:04 -0500

    updated footer

--- src/components/layouts/footer.jsx
+++ src/components/layouts/footer.jsx
@@ -6,9 +6,9 @@ export default function Footer({ }) {
   return (
     <footer className="footer" id="footer">
       <div className="" style={{ display: "flex" }}>
-        
-          <div className="py-4 px-lg-20 justify-content-around align-content-center flex-1 flex col" style={{display: 'flex', flex: '1', flexDirection: 'column'}}>
-            {footer.sections.map((section, i) => (
+
+        <div className="py-2 px-lg-20 justify-content-around align-content-center flex-1 flex col" style={{ display: 'flex', flex: '1', flexDirection: 'column' }}>
+          {/* {footer.sections.map((section, i) => (
               <div className="row order-1 mb-4" key={i}>
                 <div className="footer-widget">  
                   <ul className="list-unstyled align-content-center">
@@ -20,29 +20,34 @@ export default function Footer({ }) {
                   </ul>
                 </div>
               </div>
-            ))}
+            ))} */}
+          <div className="h-full p-4 flex col relative justify-content-around align-content-center" style={{ display: 'flex', flexDirection: 'column' }}>
+            <img src="/images/logo-romoredes-nofondo.png" alt="" width={"200px"} />
+            <p className="text-white">{footer.description}</p>
           </div>
 
-          <div className="py-4 px-lg-14 justify-content-center flex-1"
-          style={{ flex: '1'}}>
-            <div className="">
-              <div className="justify-content-around" style={{ display: 'flex', flexDirection: 'column' }}>
-                <p className="text-white">Romoredes - NIT: 901527987</p>
-                <p className="text-white">Teléfono: 3204242865 - 3182309432</p>
-                <p className="text-white"> Correo electrónico: romored3s@gmail.com</p>
-              </div>
-            </div>
-            <div className="col-12 col-lg-12 me-auto order-2 order-lg-1">
-              <div className="mt-4 mt-md-0">
 
-                <p className="text-white">
-                  © Copyright <span>{new Date().getFullYear()}</span>{" "}
-                  {footer.copyright}
-                </p>
-              </div>
+        </div>
+
+        <div className="p-8 px-lg-14 justify-content-around align-content-center flex-1"
+          style={{ flex: '1' }}>
+          <div className="">
+            <div className="justify-content-around" style={{ display: 'flex', flexDirection: 'column' }}>
+              <p className="text-white">NIT: 901527987</p>
+              <p className="text-white">Teléfono: 3204242865 - 3182309432</p>
+              <p className="text-white"> Correo electrónico: romored3s@gmail.com</p>
             </div>
           </div>
-       
+          <div className="col-12 col-lg-12 me-auto order-2 order-lg-1">
+          </div>
+        </div>
+
+      </div>
+      <div className="border-top h-full">
+        <p className="text-white mb-0 p-4 text-center">
+          © Copyright <span>{new Date().getFullYear()}</span>{" "}
+          {footer.copyright}
+        </p>
       </div>
     </footer>
   );
