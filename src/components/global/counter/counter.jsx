39a093c24ea0aabd230a01af451d0f5177964bae
sarasarixsara <sara.cuy.ramirez@gmail.com>
2025-06-27 08:42:31 -0500

    delete mapa

--- src/components/global/counter/counter.jsx
+++ src/components/global/counter/counter.jsx
@@ -52,14 +52,14 @@ export default function GlobalCounter(block) {
             className="col-lg-6"
             style={{ display: "flex", flexWrap: "wrap" }}
           >
-            <iframe
+            {/* <iframe
               src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3976.7828030807814!2d-74.07383658161287!3d4.6327996523906725!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8e3f9a2c89b424fb%3A0x5b5e23f29cee19c2!2sAsociaci%C3%B3n%20FAC%20Nuestra%20Se%C3%B1ora%20de%20Loreto!5e0!3m2!1ses!2sco!4v1750954430200!5m2!1ses!2sco"
               width="500"
               height="350"
           
               loading="lazy"
             
-            ></iframe>
+            ></iframe> */}
           </div>
 
           <div className="col-12 pb-8 mt-4">
