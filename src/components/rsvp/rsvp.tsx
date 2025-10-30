1df84c68ff0cc330722a71cb54139a3f2915207a
sarasarixsara <sara.cuy.ramirez@gmail.com>
2025-10-30 17:50:13 -0500

    icono telefono

--- src/components/rsvp/rsvp.tsx
+++ src/components/rsvp/rsvp.tsx
@@ -110,7 +110,7 @@ export default function Rsvp(block: any) {
                   {/* <svg className="rsvp__button-icon" fill="none" stroke="currentColor" viewBox="0 0 24 24" strokeWidth={2}>
                     <path strokeLinecap="round" strokeLinejoin="round" d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
                   </svg> */}
-                   O llámanos al: <span className="rsvp__phone-number">{block.phone}</span>
+                   📞
                   
                   <div className="rsvp__button-sparkle" />
                 </div>
